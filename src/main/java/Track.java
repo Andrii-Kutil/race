61fbfe81e74b058c7952108dbd30f491ea31be1a
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-01 14:15:25 +0300

    added 2 threads and main classes

--- src/main/java/Track.java
+++ src/main/java/Track.java
@@ -1,5 +1,21 @@
 public class Track {
-    public static void main(String[] args) {
+    public static int counter = 0;
 
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            while (counter <= 100) {
+                System.out.println(Thread.currentThread().getName() + " value = " + counter++);
+            }
+        });
+        Thread threadImplRunnable = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (counter <= 100) {
+                    System.out.println(Thread.currentThread().getName() + " value = " + counter++);
+                }
+            }
+        });
+        thread.start();
+        threadImplRunnable.start();
     }
 }
