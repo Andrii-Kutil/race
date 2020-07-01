37070c2fc2ae9e91ec9254eeb6c01a80dfed2fab
Andrii <61060058+Andrii-Kutil@users.noreply.github.com>
2020-07-01 20:59:54 +0300

    added 2 threads and main classes (#1)
    * added 2 threads and main classes
    
    * added 2 threads and main classes
    
    Co-authored-by: Superman696 <61060058+Superman696@users.noreply.github.com>

--- src/main/java/Track.java
+++ src/main/java/Track.java
@@ -1,5 +1,24 @@
-public class Track {
-    public static void main(String[] args) {
+import org.apache.log4j.Logger;
 
+public class Track {
+    public static int counter = 0;
+    private static final Logger logger = Logger.getLogger(Track.class);
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            while (counter <= 100) {
+                logger.info(Thread.currentThread().getName() + " value = " + counter++);
+            }
+        });
+        Thread threadImplRunnable = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (counter <= 100) {
+                    logger.info(Thread.currentThread().getName() + " value = " + counter++);
+                }
+            }
+        });
+        thread.start();
+        threadImplRunnable.start();
     }
 }
